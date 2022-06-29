edd305fa7e294adef1783e015e8c923905770811
Frankie <1426203851@qq.com>
2022-06-29 18:05:24 +0800

    test: 新增测试用例

--- bin/gest.sh
+++ bin/gest.sh
@@ -5,7 +5,6 @@ set -e
 function main() {
   local print_str=""
   local output_dir="/Users/frankie/Desktop/gif-test/dist"
-  # index_dir="$PWD/*.gif"
 
   for file in /Users/frankie/Desktop/gif-test/end/*.gif; do
     if [ -f "$file" ]; then
@@ -22,12 +21,48 @@ function main() {
       # 图片字节数
       # echo $(wc $file | awk '{print $3}')
 
-      # 效果不明显，1% ~ 2%
+      # 效果不明显，减少 1% ~ 2%
       # gifsicle "$source_file" -o "$target_file" -O3
 
-      # 效果较明显，有 12% ~ 20%
-      gifsicle "$source_file" -o "$target_file" -O3 --lossy
+      # 效果较明显，减少 12% ~ 14%
+      # gifsicle "$source_file" -o "$target_file" --lossy
 
+      # 效果较明显，减少 12% ~ 20%
+      # gifsicle "$source_file" -o "$target_file" -O3 --lossy # 其中 lossy 默认 20
+      # gifsicle "$source_file" -o "$target_file" -O3 --lossy=100 # ，往上调参数，压缩大小有明显变化，同时质量也差得比较明显
+
+      # 几乎无效果，只能减少 1Kb ~ 2Kb
+      # gifsicle "$source_file" -o "$target_file" --no-extensions
+
+      # 源文件帧数
+      source_file_frame_num=$(gifsicle "$source_file" -I | head -n 1 | awk '{print $3}')
+
+      # 抽去第一帧，降低 12% ~ 32%，但质量极差，猜测是原图经过压缩出来，将大部分不变的区域到放在了第一帧上面。
+      # 因此不建议抽调第一帧
+      # gifsicle "$source_file" -o "$target_file" --delete "#0"
+
+      # 删除第二帧
+      # gifsicle "$source_file" -o "$target_file" --delete "#1"
+
+      # 抽调奇数帧，降低 40% ~ 50%，导致速度加快的同时，而且动画很可能不衔接，可能质量较差
+      gifsicle "$source_file" $(seq -f "#%g" 0 2 "$((source_file_frame_num - 1))") >"$target_file"
+
+      # i=0
+      # cp "$source_file" "$output_dir/$(basename "$file")"
+      # while [[ $i -lt $source_file_frame_num ]]; do
+      #   rem=$((i % 2))
+      #   if [ $rem -eq 1 ]; then
+      #     gifsicle "$target_file" -o "$target_file" --delete "#$((i / 2))"
+      #   fi
+      #   i=$((i + 1))
+      # done
+
+      # 目标文件帧数
+      # target_file_frame_num=$(gifsicle "$target_file" -I | head -n 1 | awk '{print $3}')
+      # echo "befor: $source_file_frame_num"
+      # echo "after: $target_file_frame_num"
+
+      # 文件字节数
       source_size_byte=$(wc "$source_file" | awk '{print $3}')
       target_size_byte=$(wc "$target_file" | awk '{print $3}')
 
@@ -41,9 +76,9 @@ function main() {
       print_str="$print_str\n\n
       $source_file:\n
       原图: $source_size_kb KB\n
-      修改: $target_size_kb KB\n
-      变化: $change_size_kb KB\n
-      变化百分比: $change_size_per%"
+      修改后: $target_size_kb KB\n
+      减少了: $change_size_kb KB\n
+      压缩比例: $change_size_per%"
     fi
   done
 
